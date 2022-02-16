2514eb6de7ac465c461e5298ff5d17c26e58760d
HelenQuang <qvha.97@gmail.com>
2022-02-16 22:52:59 +0200

    HTML CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+const amountBox = document.querySelector(".amount-box");
+const currencyFrom = document.querySelector(".currency-from");
+const currencyTo = document.querySelector(".currency-to");
+const switchButton = document.querySelector(".btn");
+const resultBox = document.querySelector(".result-box");
